14e9ff6811bfac2196dd946416b3c5ed9bfe9d82
Pablo Álvarez <pablo.alvarezpanes949@comunidadunir.net>
2025-02-23 04:59:38 +0100

    Capturamos la información del formulario en un nuevo array newNotice

--- src/app/components/notice-form/notice-form.component.ts
+++ src/app/components/notice-form/notice-form.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { INotices } from '../../interfaces/inotices.type=interface';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-notice-form',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './notice-form.component.html',
   styleUrl: './notice-form.component.css'
 })
 export class NoticeFormComponent {
+  newNotice: INotices = {title: "", image: "", noticeBody: "", date: new Date()};
 
+  getNotice() {
+    console.log(this.newNotice);
+  }
 }
