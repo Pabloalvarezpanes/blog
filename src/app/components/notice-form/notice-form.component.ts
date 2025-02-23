c4eecd63f133dd077c8e881d9a464e46db8c9eb0
Pablo Álvarez <pablo.alvarezpanes949@comunidadunir.net>
2025-02-23 14:12:29 +0100

    Creamos la CeckControl y constructor que controla y valida los datos agregados en el formulario

--- src/app/components/notice-form/notice-form.component.ts
+++ src/app/components/notice-form/notice-form.component.ts
@@ -1,20 +1,42 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { INotices } from '../../interfaces/inotices.type=interface';
-import { FormsModule, NgForm} from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, NgForm, ReactiveFormsModule, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-notice-form',
-  imports: [FormsModule],
+  imports: [FormsModule,ReactiveFormsModule],
   templateUrl: './notice-form.component.html',
   styleUrl: './notice-form.component.css'
 })
 export class NoticeFormComponent {
   newNotice: INotices = {title: "", image: "", noticeBody: "", date: ""};
   @Output() sendNotice: EventEmitter<INotices> = new EventEmitter();
+  userForm: FormGroup;
 
-  getNotice(noticeForm: NgForm) {
-    let newNotice: INotices = noticeForm.value;
+  constructor() {
+    this.userForm = new FormGroup({
+      title: new FormControl("", [
+        Validators.required,
+        Validators.minLength(5)
+      ]),
+      image: new FormControl("",[]),
+      noticeBody: new FormControl("",[]),
+      date: new FormControl("",[
+        Validators.required,
+        Validators.pattern(/^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/\d{4}$/)
+      ])
+    }, [])
+  }
+
+  getNotice() {
+    console.log(this.newNotice)
     this.sendNotice.emit(this.newNotice);
     this.newNotice = {title: "", image: "", noticeBody: "", date: ""};
+  
+  }
+
+  checkControl(controlName:string, errorName:string): boolean | undefined{
+    return this.userForm.get(controlName)?.hasError(errorName) && this.userForm.get(controlName)?.touched
+
   }
 }
